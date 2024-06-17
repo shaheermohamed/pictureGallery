9eb62bff161cd43bc1eee2fc64462b4d17ab4998
Mohamed Shaheer <shaheer@coderythm.com>
2024-06-17 15:11:31 +0400

    feat:added setup server jwt authentication

---
+++ server/controllers/authController.js
@@ -0,0 +1,35 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const register = async (req, res) => {
+  try {
+    const { businessName, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ businessName, email, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Registration failed" });
+  }
+};
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: "Login failed" });
+  }
+};
+
+module.exports = { register, login };
--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const authRoutes = require("./routes/auth");
+const protectedRoutes = require("./routes/protectedRoute");
+const connectDb = require("./config/db");
 
 dotenv.config();
 const app = express();
 const Port = process.env.PORT;
 
-app.use(cors());
+connectDb();
 
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/protected", protectedRoutes);
 app.get("/", (req, res) => {
   res.send("server is running on" + " " + Port);
 });
