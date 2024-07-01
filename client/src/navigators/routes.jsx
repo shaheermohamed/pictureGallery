e43aea87505eb2c973639a824d5a941611849709
Mohamed Shaheer <shaheer@coderythm.com>
2024-07-01 14:49:22 +0400

    fix: redirecting bugs in client

--- client/src/navigators/routes.jsx
+++ client/src/navigators/routes.jsx
@@ -1,9 +1,10 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import View from "../pages/View";
 const routes = () => {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Navigate to="/:id" />} />
         <Route path="/:id" element={<View />} />
       </Routes>
     </div>
